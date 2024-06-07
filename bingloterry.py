f3d987519199a53b9fd64761bbb84e761cb4867c
unknown <antonio_martinez88@hotmail.com>
2024-06-07 12:31:36 -0400

    upd

--- bingloterry.py
+++ bingloterry.py
@@ -20,7 +20,9 @@ driver = webdriver.Edge()
 
 # Abre Excel file
 file_path = r".\bingloterry.csv"
+file_path_results = r".\bingloterry_results.csv"
 df = pd.read_csv(file_path)
+df_results = pd.read_csv(file_path_results)
 
 # Crea una nueva columna para las direcciones
 df["AG"] = ""
@@ -28,31 +30,19 @@ df["AG"] = ""
 # Itera a través de las filas del DataFrame
 for index, row in df.iterrows():
     name = row['Search']
-    new_latitude = row['Latitude']
-    new_longitude = row['Longitude']
+    #new_latitude = row['Latitude']
+    #new_longitude = row['Longitude']
     state = row['State']
     city = row['City']
     checked = row['found']
     if checked != "true":
         # Abre Google Maps y busca el casino
-        url = f"https://www.google.com/maps/search/{name}+{state}"
-        #url = f"https://www.bing.com/maps/?cp={new_latitude}%7E{new_longitude}&lvl=17.4"
+        url = f"https://www.google.com/maps/search/{name}+{state}"        
         driver.get(url)
 
         # Espera a que la página cargue completamente (puedes ajustar el tiempo si es necesario)
         driver.implicitly_wait(10)
-        # Obtén el tamaño de la ventana del navegador
-        """ window_size = driver.get_window_size()
-
-        # Calcula las coordenadas del centro de la ventana
-        center_x = window_size['width'] / 2
-        center_y = window_size['height'] / 2
-
-        # Intenta encontrar la dirección en la página
-        # Obtén la URL actual del navegador
-        # Mueve el cursor al centro de la pantalla
-        ActionChains(driver).move_by_offset(center_x, center_y).click().perform()
-        current_url = driver.current_url """
+        
         current_url_before_click = driver.current_url
 
         # Hacer clic en cualquier parte de la pantalla (por ejemplo, el centro)
@@ -79,6 +69,38 @@ for index, row in df.iterrows():
             print("current_url: ", current_url)
             #print(current_url)
             time.sleep(1)
+            #.fontHeadlineSmall #Contiene los nombres de las tiendas de loterry
+            #.section-subtitle-extension #el 2do  div hermano contiene la información de dirección
+
+            # Extrae el contenido de la página
+            page_source = driver.page_source
+            page_soup = soup(page_source, "html.parser")
+
+            # Encuentra los nombres de las tiendas
+            store_names = [store.get_text() for store in page_soup.find_all('div', class_='fontHeadlineSmall')]
+
+            # Encuentra las direcciones
+            address_elements = page_soup.find_all('div', class_='section-subtitle-extension')
+            addresses = []
+            for element in address_elements:
+                # Obtiene dos divs hermanos después del actual
+                sibling = element.find_next_siblings('div', limit=2)
+                if len(sibling) >= 2:
+                    address = sibling[1].get_text()+" "  # Segundo hermano
+                    addresses.append(address)
+
+            print(f"Store Names: {store_names}")
+            print(f"Addresses: {addresses}")
+
+            # Actualiza el DataFrame con los nombres y direcciones encontrados
+            for store_name, address in zip(store_names, addresses):
+                new_row = pd.DataFrame({
+                    'Name': [store_name],
+                    'Detail': [address],
+                    'URL': [current_url]                    
+                })
+                df_results = pd.concat([df_results, new_row], ignore_index=True)
+            
 
             match = re.search(r'@([+-]?\d+\.\d+),([+-]?\d+\.\d+)', current_url)
             latitude = 0
@@ -86,24 +108,24 @@ for index, row in df.iterrows():
             if match:
                 latitude = float(match.group(1))
                 longitude = float(match.group(2))
-                df.at[index, 'found'] = "true"
+                #df.at[index, 'found'] = "true"
             # Almacena la latitud y la longitud en el DataFrame
-            df.at[index, 'Latitude'] = latitude
-            df.at[index, 'Longitude'] = longitude
+            df_results.at[index, 'URL'] = current_url
+            #df_results.at[index, 'Longitude'] = longitude
+            #df_results.at[index, 'Latitude'] = longitude
             
-            df.to_csv(file_path, index=False)
-
-            #print(row)
-            print(f"Name: {row['Search']}, State: {row['State']}, Latitude: {latitude}, Longitude: {longitude}, AG: {row['AG']}")
+            df_results.to_csv(file_path_results, index=False)
+         
+            #print(f"Name: {row['Search']}, State: {row['State']}, Latitude: {latitude}, Longitude: {longitude}, AG: {row['AG']}")
         except Exception as e:
             print(str(e))
-            #print(f"No se pudo encontrar la dirección para en {state}: {e}")
-        
-        #print(df[['Name', 'Latitude', 'Longitude', 'AG']])      
-
+         
 # Cierra el navegador
 time.sleep(5)
 driver.quit()
 
+# Elimina filas duplicadas basadas en 'Store_Name' y 'Detail'
+df_results = df_results.drop_duplicates(subset=['Name', 'Detail'])
+
 # Guarda los cambios en el Excel file
-df.to_csv(file_path, index=False)
+df_results.to_csv(file_path_results, index=False)
