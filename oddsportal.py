b0e23f077213e8c5d565e9433775414229299968
unknown <antonio_martinez88@hotmail.com>
2024-06-12 11:34:43 -0400

    upd

--- oddsportal.py
+++ oddsportal.py
@@ -13,7 +13,8 @@ from bs4 import BeautifulSoup as soup #pip install beautifulsoup4
 from bs4 import SoupStrainer
 import requests
 
-# Create service for microsoft edge
+# Create service for microsoft edge driver, if dont have download it from https://developer.microsoft.com/zh-cn/microsoft-edge/tools/webdriver/?form=MA13LH#downloads
+# and change the path to the microsoft edge driver, but easily put the driver in the same directory of this script
 service = Service(verbose = True)
 options = Options()
 options.binary_location = r".\msedgedriver.exe"
@@ -21,6 +22,52 @@ driver = webdriver.Edge()
 
 
 url = "https://www.oddsportal.com/football/usa/mls/"
+
+#Go to the website, will open microsoft edge driver
+driver.get(url)
+
+# Just a waiting time to load content of the website
+driver.implicitly_wait(10)
+
+#obtain URL just to compare
+current_url_before_click = driver.current_url
+
+try:
+    time.sleep(2)
+    current_url = driver.current_url
+    print("current_url: ", current_url)
+
+    # Parsing the context
+    page_source = driver.page_source
+    page_soup = soup(page_source, "html.parser")
+
+    # find all div with the following class that contains the div with the "table" but it has more div
+    pseudo_table = page_soup.find("div", class_="min-h-[86px]")
+
+    # select the next inner div that has the table
+    child_divs = pseudo_table.find("div", recursive=False)
+
+    #print("table: \n", child_divs)
+    
+    data = []
+
+    
+    if child_divs:
+        internal_divs = child_divs.find_all("div", recursive=False)
+        for internal_div in internal_divs:
+            # iterate over the children looking for the class "bg-gray-light" (bootstrap) because contains the date
+            gray_light_div = internal_div.find("div", class_=lambda x: x and "bg-gray-light" in x.split())
+            if gray_light_div:
+                date_div = gray_light_div.find("div", recursive=False)
+                #print(gray_light_div)
+                date = date_div.text.strip()  # Suponiendo que la fecha está en texto plano dentro del div
+                data.append(date)
+
+    print(data)
+
+
+except Exception as error:
+    print("Error browsing: ", str(error))
 
 response = requests.get(url)
 print(response)
